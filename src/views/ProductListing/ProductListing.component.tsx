4a92a32746b987131682a4134a0171f0c506301a
S4d3ngineer <adam@arkuszynski@gmail.com>
2022-10-14 14:00:22 +0200

    Create ProductListing

---
+++ src/views/ProductListing/ProductListing.component.tsx
@@ -0,0 +1,40 @@
+import ProductCard from "components/product-card/ProductCard.component";
+import React from "react";
+import { ProductCardData } from "./ProductCardData";
+import * as S from "./ProductListing.styles";
+
+interface Props {
+  productList: ProductCardData[] | null | undefined;
+}
+
+export default class ProductListing extends React.Component<Props> {
+
+  renderProductList() {
+    console.log(this.props.productList);
+    const productListContent = this.props.productList?.map((product, index) => 
+      <ProductCard img={product.image} name={product.name} price={product.price} key={index} />
+    )
+    return (
+      <React.Fragment>
+        {productListContent}
+      </React.Fragment>
+    ) 
+  }
+
+  componentDidMount(): void {
+    this.renderProductList();
+  }
+
+  render() {
+    return (
+      <S.Container>
+        <S.HeadingContainer>
+          <h2>all</h2>
+        </S.HeadingContainer>
+        <S.ProductList>
+          {this.renderProductList()}
+        </S.ProductList>
+      </S.Container>
+    ) 
+  } 
+}
---
+++ src/views/ProductListing/ProductListing.styles.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 5vw 0;
+`
+
+export const HeadingContainer = styled.div`
+  display: flex;
+  align-self: start;
+  max-width: 1440px;
+  padding-left: 80px;
+  padding-bottom: 5vw;
+  margin: 0 auto;
+  @media (max-width: 1440px) {
+    padding-left: 6vw;
+  }
+  h2 {
+    text-transform: capitalize;
+    font-weight: 400;
+  }
+`
+
+
+export const ProductList = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 1440px;
+  margin: 0 auto;
+  row-gap: calc(0.15 * 1440px);
+  column-gap: calc(0.05 * 1440px);
+  @media (max-width: 1440px) {
+    row-gap: 15vw;
+    column-gap: 5vw;
+  }
+` 
